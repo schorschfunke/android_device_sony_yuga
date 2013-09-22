0c6f1406924d20b07e8cf35317beea5cf981da57
schorschfunke <schorsch.funke@googlemail.com>
2013-09-22 07:44:22 +0200

    Revert "yuga: switch to mr2 audio"
    This reverts commit 53a3ef4b5abb25c14182c57e7e83a8c94f7b5a9a.

--- yuga.mk
+++ yuga.mk
@@ -61,7 +61,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3
 
 # Touchpad
 PRODUCT_COPY_FILES += \
