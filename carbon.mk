08596ec1b5d385ff8ca0761050c9cbadfbe15665
schorschfunke <schorsch.funke@googlemail.com>
2013-11-05 18:20:23 +0100

    Remove unused locales

--- carbon.mk
+++ carbon.mk
@@ -31,7 +31,7 @@ $(call inherit-product, vendor/carbon/config/common_gsm.mk)
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # languages
-PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+PRODUCT_LOCALES := en_US de_DE en_GB
  
 #prop overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
