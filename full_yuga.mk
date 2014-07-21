90def9043070d0c027d90baa25e4c807cce7e4d2
schorschfunke <schorsch.funke@googlemail.com>
2014-07-21 06:06:59 +0200

    Add QuickBoot

--- full_yuga.mk
+++ full_yuga.mk
@@ -18,9 +18,12 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
  
-# Torch
+# Extra Packages
 PRODUCT_PACKAGES := \
-    Torch
+    Torch \
+    QuickBoot
+
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
